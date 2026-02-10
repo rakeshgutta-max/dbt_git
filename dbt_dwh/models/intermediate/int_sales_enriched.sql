d48b3fad765c6ecea1b2ad9de410091773d7971e
Rakesh Gutta <rakesh.gutta@tigeranalytics.com>
2026-02-10 15:24:07 +0530

    Auto-fix: dbt validator suggestions

--- dbt_dwh/models/intermediate/int_sales_enriched.sql
+++ dbt_dwh/models/intermediate/int_sales_enriched.sql
@@ -1,3 +1,6 @@
+-- Model Name   : int_sales_enriched
+-- Purpose      : The purpose of the "int_sales_enriched" dbt model is to enrich the sales data by converting the sale amounts to USD and including additional relevant information from the "stg_sales" source, providing a comprehensive view of sales performance.
+-- Source Models: stg_sales
 select
     s.*,
     {{ convert_to_usd('sale_amount','currency') }} as sale_usd
